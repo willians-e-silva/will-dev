95af6703d2f75ef4fd615099d5e318d180ac246f
Willians E. Silva <willians.silva9620@gmail.com>
2024-09-28 16:27:18 -0300

    header changes

--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -2,12 +2,12 @@ import styles from './hello-world.module.scss';
 
 export function Header() {
   return (
-    <div className='flex'>
+    <div className='flex font-poppins px-16 py-4'>
       <div>
-        <h1 className='text-white'>Will Dev</h1>
+        <h1 className='text-white font-bold text-2xl'>Will Dev</h1>
       </div>
 
-      <nav className='ml-auto pr-24'>
+      <nav className='ml-auto'>
         <ul className='flex text-white gap-16'>
           <li><a href="">Home</a></li>
           <li><a href="">About Me</a></li>
